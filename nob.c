635d79b4cfa6dc3c5bad5e166c14090f0f42ad34
puugz <matej1.svetina@gmail.com>
2025-01-13 22:19:56 +0100

    first commit

---
+++ nob.c
@@ -0,0 +1,19 @@
+#define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+#include "nob.h"
+
+#define BUILD_FOLDER "build/"
+
+int main(int argc, char **argv) {
+  NOB_GO_REBUILD_URSELF(argc, argv);
+
+  Cmd cmd = {0};
+  cmd_append(&cmd, "cl", "/nologo", "/Zi", "..\\src\\win32_handmade.cpp");
+  cmd_append(&cmd, "user32.lib");
+
+  if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+  if (!set_current_dir(BUILD_FOLDER))     return 1;
+  if (!cmd_run_sync_and_reset(&cmd))      return 1;
+
+  return 0;
+}
